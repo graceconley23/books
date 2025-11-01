ba47e5b097a41e54ad65685fa1ca784c7d74905e
graceconley23 <graceconley23@gmail.com>
2025-11-01 12:16:19 -0400

    Initialized react bootstrap project I think

---
+++ book-react/src/App.jsx
@@ -0,0 +1,12 @@
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>"Welcome Bookworms!"</h1>
+      <Button variant="success">Read!</Button>
+    </div>
+  );
+}
+
+export default App;
