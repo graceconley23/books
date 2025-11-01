b231170e4731541a9545872b298d130135b2d36b
graceconley23 <graceconley23@gmail.com>
2025-11-01 12:51:30 -0400

    usable button

--- book-react/src/App.jsx
+++ book-react/src/App.jsx
@@ -1,12 +1,23 @@
 import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  function increaseCount() {
+    setCount(count + 1);
+  }
+
   return (
-    <div className="App">
-      <h1>"Welcome Bookworms!"</h1>
-      <Button variant="success">Read!</Button>
-    </div>
-  );
-}
+    <div className="d-grid gap-2">
+       <h1>"Welcome Bookworms!"</h1>
+       <Button variant="outline-success" size="lg" onClick={increaseCount}>
+          Read!
+       </Button>
+       <h2>You’ve read {count} times this year</h2>
+     </div>
+   );
+ }
 
 export default App;
