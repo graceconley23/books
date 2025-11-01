f5ec7e3c6d959ee4025cc14939d75b43fcf57080
graceconley23 <graceconley23@gmail.com>
2025-11-01 18:29:38 -0400

    oopsies my bad gang now we have a search bar

--- book-react/src/components/SearchBar.jsx
+++ book-react/src/components/SearchBar.jsx
@@ -2,19 +2,25 @@ import { Component } from "react";
 import Form from 'react-bootstrap/Form';
 
 class SearchBar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            inputText: ""
-        };
-    }
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         inputText: ""
+    //     };
+    // }
+
+    handleKeyPress = (event) => {
+        if (event.key === 'Enter') {
+            alert("Hitched Enter! You searched for: " + event.target.value);
+        }
+    };
     
     render() {
         return (
             <Form>
                 <Form.Group className="mb-3" controlId="my-search-bar">
                 <Form.Label>Search the Catalog</Form.Label>
-                <Form.Control placeholder="Search by title, author, or series" />
+                <Form.Control placeholder="Search by title, author, or series" onKeyDown={this.handleKeyPress}/>
                 </Form.Group>
             </Form>
         );
