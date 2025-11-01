330fba6e9d4f0bff66e0def720d2f33e1da33ee0
NerdyBread42 <samfondacaro2006@gmail.com>
2025-11-01 18:51:28 -0400

    Created CatalogDAO/CatalogFileDAO; filled out User operations and added missing methods

---
+++ bookstore-backend/book/src/main/java/com/book/book/persistence/CatalogFileDAO.java
@@ -0,0 +1,173 @@
+package com.book.book.persistence;
+
+import com.book.book.model.Book;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
+@Component
+public class CatalogFileDAO implements CatalogDAO {
+    public File file;
+    public ObjectMapper objectMapper;
+    private TreeMap<String, Book> bookMap;
+
+    public CatalogFileDAO(@Value("${books.file}") String filename, ObjectMapper objectMapper) {
+        this.file = new File(filename);
+        this.objectMapper = objectMapper;
+        loadFromFile();
+    }
+
+    private void loadFromFile() {
+        this.bookMap = new TreeMap<>();
+        try {
+            Book[] booksArray = objectMapper.readValue(file, Book[].class);
+
+            for (Book book : booksArray) {
+                this.bookMap.put(book.getISBN(), book);
+            }
+        } catch (IOException e) {
+            System.out.println("No books to load");
+        }
+    }
+
+    private void writeToFile() throws IOException {
+        objectMapper.writeValue(file, getBooksArray());
+    }
+
+    private Book[] getBooksArray() {
+        ArrayList<Book> temp = new ArrayList<>(bookMap.values());
+
+        Book[] ret = new Book[temp.size()];
+        return temp.toArray(ret);
+    }
+
+    // TODO sort by author, series, title
+    private ArrayList<Book> getBooksArrayByText(String searchText) {
+        ArrayList<Book> temp = new ArrayList<>(bookMap.values());
+
+        for (Book book : bookMap.values()) {
+            if (book.getTitle().toLowerCase().contains(searchText.toLowerCase())) {
+                temp.add(book);
+            } else if (book.getAuthor().toLowerCase().contains(searchText.toLowerCase())) {
+                temp.add(book);
+            } else if (book.getSeries().toLowerCase().contains(searchText.toLowerCase())) {
+                temp.add(book);
+            }
+        }
+
+        return temp;
+    }
+
+    private Book[] filterSearch(ArrayList<Book> books, double minPrice, double maxPrice,
+                                List<String> genres, boolean checkInStock) {
+        boolean checkingGenres = !genres.isEmpty();
+        boolean removeFlag;
+
+        for (Book book : books) {
+            if (minPrice != 0 && book.getPrice() < minPrice) {
+                books.remove(book);
+            } else if (maxPrice != 0 && book.getPrice() > maxPrice) {
+                books.remove(book);
+            } else if (checkingGenres) {
+                removeFlag = true;
+                for (String genre : genres) {
+                    if (book.isGenre(genre)) {
+                        removeFlag = false;
+                        break;
+                    }
+                }
+                if (removeFlag) {
+                    books.remove(book);
+                }
+            }
+            else if (checkInStock && book.outOfStock()) {
+                books.remove(book);
+            }
+        }
+
+        Book[] ret =  new Book[books.size()];
+        return books.toArray(ret);
+    }
+
+    @Override
+    public Book[] getBooks() throws IOException {
+        return getBooksArray();
+    }
+
+    @Override
+    public Book[] searchBook(String searchText, double minPrice, double maxPrice,
+                             List<String> genres, boolean inStock) throws IOException {
+        return filterSearch(getBooksArrayByText(searchText), minPrice, maxPrice, genres, inStock);
+    }
+
+    @Override
+    public Book getBook(String ISBN) throws IOException {
+        return bookMap.get(ISBN);
+    }
+
+    @Override
+    public Book createBook(Book book) throws IOException {
+        if (!bookMap.containsKey(book.getISBN())) {
+            return null;
+        }
+        bookMap.put(book.getISBN(), book);
+        writeToFile();
+        return book;
+    }
+
+    @Override
+    public Book updateBook(String ISBN, Book book) throws IOException {
+        if (!bookMap.containsKey(book.getISBN())) {
+            return null;
+        }
+        book.setISBN(ISBN);
+        bookMap.put(book.getISBN(), book);
+        writeToFile();
+        return book;
+    }
+
+    @Override
+    public Book deleteBook(String ISBN) throws IOException {
+        if (!bookMap.containsKey(ISBN)) {
+            return null;
+        }
+        bookMap.remove(ISBN);
+        writeToFile();
+        return bookMap.get(ISBN);
+    }
+
+    @Override
+    public Book increaseStock(String ISBN, int amount) throws IOException {
+        if (!bookMap.containsKey(ISBN)) {
+            return null;
+        }
+        Book book = bookMap.get(ISBN);
+        book.increaseQuantity(amount);
+        writeToFile();
+        return book;
+    }
+
+    @Override
+    public Book decreaseStock(String ISBN, int amount) throws IOException {
+        if (!bookMap.containsKey(ISBN)) {
+            return null;
+        }
+        if (bookMap.get(ISBN).outOfStock()) {
+            // can't decrease the stock of a book if there are already 0 copies
+            return null;
+        }
+        Book book = bookMap.get(ISBN);
+        book.decreaseQuantity(amount);
+        if (book.getQuantity() < 0) {
+            book.setQuantity(0);
+        }
+        writeToFile();
+        return book;
+    }
+}
