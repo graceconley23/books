2e0d7265ee07f7a85076af5bc564798b4848d84c
graceconley23 <graceconley23@gmail.com>
2025-11-02 03:17:58 -0500

    started cart

---
+++ book-react/src/components/Cart.jsx
@@ -0,0 +1,59 @@
+import BookCard from "./BookCard.jsx";
+
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+function Cart() {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  async function fetchBooks() {
+    try {
+      const cart = await axios.get(`http://localhost:8080/cart/1`).then(response => response.data);
+      setBooks(cart);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => { // call once on startup
+    fetchBooks();
+  }, [])
+
+  useEffect(() => {
+    fetchBooks();
+  })
+
+  if (loading) return <p>Loading cart...</p>
+
+  return ( 
+    <div>
+      {/* Main content area */}
+      <div className="d-flex">
+        {/* Left content: Cards */}
+        <div className="flex-grow-1 p-4">
+          <div className="d-flex flex-wrap justify-content-start gap-4">
+            {books.map((book, index) => (
+              <BookCard
+                key={index}
+                title={book.title}
+                author={book.author}
+                series={book.series}
+                volume={book.numberInSeries}
+                cover={book.coverImageUrl}
+              />
+            ))}
+          </div>
+        </div>
+        {/* Right content: Order Summary */}
+        <div className="p-3 border-end" style={{ width: "20rem", minHeight: "100vh" }}>
+          <h4>Order Summary</h4>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cart;
